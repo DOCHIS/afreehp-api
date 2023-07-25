d43986849bc78a852348b0e6788bb5b1c47b25ad
dochis2013 <dochis2013@gmail.com>
2023-07-25 15:06:56 +0900

    Fix: 오타수정

--- handler/logger.js
+++ handler/logger.js
@@ -12,7 +12,7 @@ exports.handler = async (event, context) => {
     const { logGroup, messageType, logStream } = payload;
     const logGroupSplit = logGroup.split("-");
     const stage = logGroupSplit[2];
-    const ttlPeriod = sage === "dev" ? 4 : 90;
+    const ttlPeriod = stage === "dev" ? 4 : 90;
     const ttl = 60 * 60 * 24 * ttlPeriod;
 
     console.log(">> data", event.awslogs.data);
